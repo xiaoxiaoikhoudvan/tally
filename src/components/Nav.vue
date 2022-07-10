c3c10bc55dc1676603253ba0109f5bbd2152fd18
xiaoxiaoikhoudvan <752998553@qq.com>
2022-07-10 17:28:32 +0800

    添加全局样式和全局变量

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,14 +1,14 @@
 <template>
     <nav>
-      <router-link to="/money" class="item">
+      <router-link to="/money" class="item" active-class="selected">
       <Icon class="" name="money" />
       记账
       </router-link>
-      <router-link to="/labels" class="item">
+      <router-link to="/labels" class="item" active-class="selected">
       <Icon name="label"/>
       标签
       </router-link>
-      <router-link to="/statistics" class="item">
+      <router-link to="/statistics" class="item" active-class="selected">
       <Icon name="statistics"/>
       统计
       </router-link>
@@ -22,6 +22,7 @@
 </script>
 
 <style lang="scss" scoped>
+  @import "~@/assets/style/helper.scss";
   nav {
       display: flex;
       box-shadow: 0 0 3px rgba(0,0,0,0.25);
@@ -38,6 +39,10 @@
           width: 32px;
           height: 32px;
         }
+    }
+    //点击导航激活的颜色
+    > .item.selected{
+      color: $color-highlight;
     }
   }
 </style>
--- vue.config.js
+++ vue.config.js
@@ -10,6 +10,9 @@ module.exports = defineConfig({
     .test(/\.svg$/)
     .include.add(dir).end() // 包含 icons 目录
     .use('svg-sprite-loader').loader('svg-sprite-loader').options({extract:false}).end()
+    //以下两行为了去掉svg中的fill颜色
+    // .use('svgo-loader').loader('svgo-loader')
+    // .tap(options => ({...options, plugins: [{removeAttrs: {attrs: 'fill'}}]})).end()
     config.plugin('svg-sprite').use(require('svg-sprite-loader/plugin'), [{plainSprite: true}])
     config.module.rule('svg').exclude.add(dir) // 其他 svg loader 排除 icons 目录
   }
