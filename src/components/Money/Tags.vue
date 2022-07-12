95ae7b56242e73688f836f05b33485554300c158
xiaoxiaoikhoudvan <752998553@qq.com>
2022-07-12 20:28:58 +0800

    先把money模块化

---
+++ src/components/Money/Tags.vue
@@ -0,0 +1,55 @@
+<template>
+    <div class="tags">
+        <div class="new">
+            <button>新增标签</button>
+        </div>
+        <ul class="current">
+            <li>衣</li>
+            <li>食</li>
+            <li>住</li>
+            <li>行</li>
+        </ul>
+    </div>
+</template>
+
+<script lang="ts">
+    export default {
+        name:'Tags'
+    }
+</script>
+
+<style lang="scss" scoped>
+    .tags {
+        font-size:14px;
+        padding: 16px;
+        flex-grow: 1;
+        display: flex;
+        flex-direction: column-reverse;
+        > .current {
+            display: flex;
+            flex-wrap: wrap;
+            > li{
+                $h: 24px;
+                background: #d9d9d9;
+                height: $h;
+                line-height: $h;
+                border-radius: $h/2;
+                padding: 0 20px;
+                margin-right: 16px;
+                margin-top: 4px;
+            }
+        }
+        > .new {
+            padding-top: 16px;
+            button{
+                //背景设置透明
+                background: transparent;
+                border: none;
+                color: #999;
+                //不给颜色 则下划线跟字体一样颜色
+                border-bottom: 1px solid;
+                padding: 0 4px;
+            }
+        }
+    }
+</style>
---
+++ src/components/Money/Types.vue
@@ -0,0 +1,42 @@
+<template>
+    <div>
+        <ul class="types">
+            <li class="selected">支出</li>
+            <li>收入</li>
+        </ul>
+    </div>
+</template>
+
+<script lang="ts">
+    export default {
+        name:'Types'
+    }
+</script>
+
+<style lang="scss" scoped>
+    .types{
+        background: #c4c4c4;
+        display: flex;
+        text-align: center;
+        font-size: 24px;
+        > li {
+            width: 50%;
+            height: 64px;
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            position: relative;
+            //表示当前被选中,用after是因为如果不用的话选中的时候由于border-bottom有高度，会导致
+            //文字向上撑   
+            &.selected::after{
+                content: '';
+                position: absolute;
+                height: 4px;
+                width: 100%;
+                bottom: 0;
+                left: 0;
+                background: #333;
+            }
+        }
+    }
+</style>
