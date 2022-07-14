78caa04e5be32edfc6bcf9267c9d9d5d567578bc
xiaoxiaoikhoudvan <752998553@qq.com>
2022-07-14 20:15:23 +0800

    完成NumberPad的数字输入

--- src/components/Money/Types.vue
+++ src/components/Money/Types.vue
@@ -1,16 +1,52 @@
 <template>
     <div>
         <ul class="types">
-            <li class="selected">支出</li>
-            <li>收入</li>
+            <li :class="type === '-' && 'selected'"
+                @click="selectType('-')">支出</li>
+            <li :class="type === '+' && 'selected'"
+                @click="selectType('+')">收入</li>
         </ul>
     </div>
 </template>
 
 <script lang="ts">
-    export default {
-        name:'Types'
+    import Vue from 'vue';
+    import {Component, Prop} from 'vue-property-decorator';
+
+    @Component
+    export default class Types extends Vue {
+        type = '-'; // '-'表示支出  '+'表示表示收入
+        
+        selectType(type:string){
+            if (type !== '-' && type !== '+') {
+                throw new Error('type is unknown')
+            }
+            this.type = type
+        }
+
+        mounted(){
+        }
     }
+    // export default {
+    //     name:'Types',
+    //     props:['xxx'],
+    //     data(){
+    //         return{
+    //             type:'-' // '-'表示支出  '+'表示表示收入
+    //         }
+    //     },
+    //     mounted(){
+    //         console.log("xxx=>",this.xxx);
+    //     },
+    //     methods:{
+    //         selectType(type){
+    //             if (type !== '-' && type !== '+') {
+    //                 throw new Error('type is unknown')
+    //             }
+    //             this.type = type
+    //         }
+    //     }
+    // }
 </script>
 
 <style lang="scss" scoped>
