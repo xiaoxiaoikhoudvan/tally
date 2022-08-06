80a87a6e8d3a8e4cc0b94c0a3ee9d7f787e89aa6
xiaoxiaoikhoudvan <752998553@qq.com>
2022-08-06 15:40:19 +0800

    修复一进页面就弹出四个创建成功的弹窗的bug

--- src/mixins/TagHelper.ts
+++ src/mixins/TagHelper.ts
@@ -9,6 +9,12 @@ export  class TagHelper extends Vue{
             return window.alert('标签名不能为空')
         }
         this.$store.commit('createTag',name);
+        console.log("state=>",this.$store.state);
+        if(this.$store.state.createTagError){
+            console.log("进来");
+            window.alert(this.$store.state.createTagError.message);
+        }
+        
     }
 }
 export default TagHelper;
--- src/store/index.ts
+++ src/store/index.ts
@@ -11,6 +11,7 @@ const store =  new Vuex.Store({
   state: {
     recordList:[],
     createRecordError:null,
+    createTagError:null,
     tagList:[],
     currentTag:undefined
   } as RootState,
@@ -71,16 +72,15 @@ const store =  new Vuex.Store({
       }
     },
     createTag(state,name:string){
+      state.createTagError = null;
       const names = state.tagList.map(item => item.name);
         if (names.indexOf(name) >= 0) {
-            window.alert('标签名重复');
-            return 'duplicated';
+            state.createTagError = new Error('标签名重复！');
+            return;
         }
         const id = createId().toString();
         state.tagList.push({id,name:name});
         store.commit('saveTags'); 
-        window.alert('添加成功');
-        return 'success';
     },
     saveTags(state){
       window.localStorage.setItem('tagList',JSON.stringify(state.tagList));
