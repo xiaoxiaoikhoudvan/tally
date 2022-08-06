32828ec3ef9771b8d443918a00684762fd79cef6
xiaoxiaoikhoudvan <752998553@qq.com>
2022-08-06 16:23:19 +0800

    部署网站到 tally-web

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,9 @@
 const { defineConfig } = require('@vue/cli-service')
 const path = require('path')
 module.exports = defineConfig({
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/tally-web/'
+    : '/',
   transpileDependencies: true,
   chainWebpack:config=>{
     const dir = path.resolve(__dirname,'src/assets/icons')
